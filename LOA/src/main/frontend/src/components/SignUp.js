5b44618a611ff600be72d441ced4464f34d55218
Dukjoon Jung <ever@Dukjoonui-MacBookAir.local>
2023-08-10 15:33:46 +0900

    issue) axios error

--- LOA/src/main/frontend/src/components/SignUp.js
+++ LOA/src/main/frontend/src/components/SignUp.js
@@ -10,7 +10,7 @@ const SignUp = () => {
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("/api/register", {
+      const response = await axios.post("/auth/sign", {
         userId,
         password,
         charName,
