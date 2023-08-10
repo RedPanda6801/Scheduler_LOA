5a995ff0598a02de2b20f2424d890c74adc9cc7d
Redpanda6801 <redpanda6801@gmail.com>
2023-08-10 15:56:16 +0900

    [Ver 1.0.5] 백엔드 및 프론트 api 수정

--- LOA/src/main/frontend/src/components/SignUp.js
+++ LOA/src/main/frontend/src/components/SignUp.js
@@ -10,7 +10,7 @@ const SignUp = () => {
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("/api/register", {
+      const response = await axios.post("/api/auth/sign", {
         userId,
         password,
         charName,
