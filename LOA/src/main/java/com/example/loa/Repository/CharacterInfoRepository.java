9b40886bb6fbd8cbdf2fe485c4606da10b768202
Redpanda6801 <redpanda6801@gmail.com>
2023-08-13 00:02:18 +0900

    [8.12/Redpanda] 스케줄 캐릭터들 정보 수정

--- LOA/src/main/java/com/example/loa/Repository/CharacterInfoRepository.java
+++ LOA/src/main/java/com/example/loa/Repository/CharacterInfoRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface CharacterInfoRepository extends JpaRepository<CharacterInfo, Integer>{
     List<CharacterInfo> findAllByUserId(Integer id);
+
+    CharacterInfo findByCharName(String charName);
 }
