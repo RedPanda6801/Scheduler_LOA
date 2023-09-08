1fa58e8c23bae2ca60a68605623e68197ac09fb1
Dukjoon Jung <ever@Dukjoonui-MacBookAir.local>
2023-09-08 13:12:53 +0900

    category5는 크루관련

--- LOA/src/main/frontend/src/components/Category5.js
+++ LOA/src/main/frontend/src/components/Category5.js
@@ -1,38 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Category4 = () => {
-  // const [userCreatedCrew, setUserCreatedCrew] = useState("");
   const [favoriteCrewMembers, setFavoriteCrewMembers] = useState([]);
   const [selectedCrewMember, setSelectedCrewMember] = useState("");
   const [contentProgress, setContentProgress] = useState("");
-
+  const [crewMembers, setCrewMembers] = useState([]);
   const token = localStorage.getItem("token");
 
-  // const handleCreateCrew = () => {
-  //   // crew api 따로 적용 할것인지..?
-  //   // 적용 안하면 Post쪽 바꿔야돼
-  //   axios
-  //     .post(
-  //       "/api/crew/create",
-  //       {},
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       }
-  //     )
-  //     .then((response) => {
-  //       setUserCreatedCrew("Crew created");
-  //     })
-  //     .catch((error) => {
-  //       setUserCreatedCrew("Crew creation failed");
-  //     });
-  // };
+  // 크루원 목록 가져오기
+  useEffect(() => {
+    axios
+      .get("/api/crew/members", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setCrewMembers(response.data.members);
+      })
+      .catch((error) => {
+        console.error("Error fetching crew members", error);
+      });
+  }, [token]);
 
   const handleFavoriteCrewMember = (crewMember) => {
-    // 즐겨찾기
-    // Example
+    // 즐겨찾기 토글
     if (favoriteCrewMembers.includes(crewMember)) {
       setFavoriteCrewMembers(
         favoriteCrewMembers.filter((member) => member !== crewMember)
@@ -43,7 +36,7 @@ const Category4 = () => {
   };
 
   const handleCrewMemberClick = (crewMember) => {
-    // 클릭 시 나오는 것들
+    // 크루원 컨텐츠 진행 현황 확인
     axios
       .get(`/api/crew/${crewMember}/content-progress`, {
         headers: {
@@ -60,26 +53,30 @@ const Category4 = () => {
 
   return (
     <div>
-      {/*<button onClick={handleCreateCrew}>크루 생성</button>*/}
-      {/*<p>{userCreatedCrew}</p>*/}
-
       <div>
         <h2>즐겨찾기 크루원</h2>
         {/* 즐겨찾기 리스트 */}
-        {favoriteCrewMembers.map((member) => (
+        {crewMembers.map((member) => (
           <div key={member}>
             {member}
             <button onClick={() => handleFavoriteCrewMember(member)}>
-              즐겨찾기
+              {favoriteCrewMembers.includes(member)
+                ? "즐겨찾기 해제"
+                : "즐겨찾기"}
             </button>
           </div>
         ))}
       </div>
 
       <div>
-        {/* 그냥 리스트업 */}
+        {/* 크루원 선택 */}
         <select onChange={(e) => setSelectedCrewMember(e.target.value)}>
           <option value="">크루원 선택</option>
+          {crewMembers.map((member) => (
+            <option key={member} value={member}>
+              {member}
+            </option>
+          ))}
         </select>
         <button onClick={() => handleCrewMemberClick(selectedCrewMember)}>
           콘텐츠 진행 현황 확인
